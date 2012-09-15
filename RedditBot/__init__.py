2ce1ebd957513a98acaa7633f9a07281946025cf
Ed Kellett <edk141@gmail.com>
2012-09-15 02:05:35 +0100

    reference badword module

--- RedditBot/__init__.py
+++ RedditBot/__init__.py
@@ -13,4 +13,4 @@ bot = Bot()
 bot.config.from_object(Config)
 
 # load our plugins
-from RedditBot.plugins import reddit, twitter, botutils, youtube
+from RedditBot.plugins import reddit, twitter, botutils, youtube, badword
