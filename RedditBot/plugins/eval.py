64cf8a6cb6ad3c12b9c39bf48c200ad88b5cfea0
Joe Alcorn <me@buttscicl.es>
2012-09-15 14:39:17 +0100

    Move eval to seperate plugin

---
+++ RedditBot/plugins/eval.py
@@ -0,0 +1,18 @@
+from RedditBot import bot, utils
+
+import sys
+import __builtin__
+
+
+@bot.command
+def eval(context):
+    '''.eval <command>'''
+    if not utils.isadmin(context.line['prefix'], bot):
+        return
+    if context.args:
+        try:
+            return str(__builtin__.eval(context.args))
+        except:
+            return repr(sys.exc_info()[1])
+    else:
+        return eval.__doc__
