1e7cbebf0d93fda38f1b849705a7a823ce0b86ae
Joe Alcorn <me@buttscicl.es>
2012-09-16 01:01:50 +0100

    Clean up google.py a bit

--- RedditBot/plugins/google.py
+++ RedditBot/plugins/google.py
@@ -1,35 +1,20 @@
 from RedditBot import bot, utils
 
-# the Google search API URL
 search_url = 'http://ajax.googleapis.com/ajax/services/search/web'
 
 
-@bot.command('g')  # also bind this function to '.g'
-@bot.command  # register the wrapped function as a plugin
+@bot.command('g')
+@bot.command
 def google(context):
     '''.g <query>'''
-    # notice that we provide one arg, context: this is optional but if you
-    # want access to the IRC line that triggered the plugin you need to
-    # pass in some variable; we'll use context for this
+    query = context.args
+    r = utils.make_request(search_url, params={'v': '1.0', 'safe': 'off', 'q': query})
 
-    query = context.args  # args are parsed automatically by IrcTK
-
-    # make the request, should give us back some JSON
-    r = utils.make_request(search_url, params=dict(v='1.0', safe='off', q=query))
-
-    # if we don't have results, bail out of the plugin
     if not r.json['responseData']['results']:
         return 'no results found'
 
-    # otherwise grab the first result
     first_result = r.json['responseData']['results'][0]
-
     title = utils.unescape_html(first_result['titleNoFormatting'])
+    ret = title + ' - ' + first_result['unescapedUrl']
 
-    # build our return string
-    ret = title + ' - ' \
-            + first_result['unescapedUrl']
-
-    # finally return the result to the channel or user the plugin was called
-    # from
-    return '{0}: {1}'.format(context.line['user'], ret)
+    return u'{0}: {1}'.format(context.line['user'], ret)
