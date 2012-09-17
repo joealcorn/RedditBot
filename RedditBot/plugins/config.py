1377b055e6b1834afa81a7d904b19a1de6aff2ae
Ed Kellett <edk141@gmail.com>
2012-09-17 04:52:58 +0100

    Add more .config options

--- RedditBot/plugins/config.py
+++ RedditBot/plugins/config.py
@@ -1,6 +1,7 @@
 
 from RedditBot import bot, utils
 
+import copy
 import yaml
 
 blacklist = ['REGEX', 'IGNORE', 'EVENTS', 'CHANNELS', 'PLUGINS']
@@ -10,7 +11,7 @@ def save_config():
         f.write(yaml.dump(dict((key, value) for key, value in bot.config.iteritems() if
             not (key.upper() in blacklist) and
             isinstance(value, list) and
-            (not bot.h_config.has_key(key) or bot.h_config[key] != value))))
+            (not key in bot.h_config or bot.h_config[key] != value))))
 
 def load_config():
     with open('bot_config.yml', 'r') as f:
@@ -24,21 +25,44 @@ load_config()
 
 @bot.command
 def config(context):
+    '''.config (view|add|remove|revert) <key> [value]'''
     if not utils.isadmin(context.line['prefix'], bot):
         return
-    cmd, key, arg = str(context.args).split(' ', 3)
+    cmd, key = str(context.args).split(' ', 1)
+    if len(key.split(' ', 1)) > 1:
+        key, arg = key.split(' ', 1)
     cmd = cmd.lower()
-    if not (cmd in ['add', 'remove']):
+    if not (cmd in ['add', 'remove', 'view', 'revert']):
         return
-    if key.upper() in blacklist or (key in bot.config and not isinstance(bot.config[key], list)):
+    if key.upper() in blacklist:
+        return
+    if cmd == 'view':
+        if key in bot.config:
+            bot.reply(repr(bot.config[key]), context.line, False, True, context.line['user'], nofilter = True)
+        return
+    elif cmd == 'revert':
+        if key in bot.h_config:
+            bot.config[key] = copy.deepcopy(bot.h_config[key])
+            save_config()
+            bot.reply(repr(bot.config[key]), context.line, False, True, context.line['user'], nofilter = True)
+        elif key in bot.config:
+            del bot.config[key]
+            save_config()
+            bot.reply('Key deleted.', context.line, False, True, context.line['user'], nofilter = True)
+        else:
+            bot.reply('Nothing to do.', context.line, False, True, context.line['user'], nofilter = True)
+        return
+    if key in bot.config and not isinstance(bot.config[key], list):
         return
     if not key in bot.config:
         bot.config[key] = []
     if cmd == 'add' and not arg in bot.config[key]:
         bot.config[key].append(arg)
-    elif cmd == 'remove' and (not bot.h_config.has_key(key) or not (arg in bot.h_config[key])):
+        save_config()
+        return 'Added.'
+    elif cmd == 'remove' and (not key in bot.h_config or not (arg in bot.h_config[key])):
         bot.config[key].remove(arg)
-        if len(bot.config[key]) == 0 and not bot.h_config.has_key(key):
+        if len(bot.config[key]) == 0 and not key in bot.h_config:
             del bot.config[key]
-    save_config()
-
+        save_config()
+        return 'Removed.'
