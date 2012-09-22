e8b43573919381b42341e50706fa4538aea4c145
Ed Kellett <edk141@gmail.com>
2012-09-22 21:49:13 +0100

    increase paste timeout to 15s

--- RedditBot/pastebin.py
+++ RedditBot/pastebin.py
@@ -4,7 +4,7 @@ from RedditBot import utils
 def paste(contents, title=None, language='text', unlisted=0, password=None):
     paste = dict([(i, locals()[i]) for i in locals().keys() if locals()[i] is not None])
     url = 'http://buttscicl.es/paste/api/add'
-    r = utils.make_request(url, params=paste, method='post')
+    r = utils.make_request(url, params=paste, method='post', timeout=15)
     if isinstance(r, str):
         return {'success': False, 'error': r}
     else:
