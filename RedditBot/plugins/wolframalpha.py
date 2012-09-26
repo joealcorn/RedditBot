53cc63b8e2aceab7923aa034e107b7f437c5694d
Ed Kellett <edk141@gmail.com>
2012-09-27 00:28:10 +0100

    improve .wa

--- RedditBot/plugins/wolframalpha.py
+++ RedditBot/plugins/wolframalpha.py
@@ -1,14 +1,46 @@
+
 from RedditBot import bot, utils
-import urllib
 
-uri = 'http://tumbolia.appspot.com/wa/'
+import BeautifulSoup
+import re
 
+url = 'http://www39.wolframalpha.com/input/'
+
+letters_re = re.compile(r'^(?:\w \| )+\w$')
+
+@bot.command('wa')
 @bot.command
-def wa(context):
-    '''.wa <query>'''
-    query = context.args
-    answer = utils.make_request(uri + urllib.quote(query.replace('+', '%2B')), timeout=10)
-    if answer: 
-       return u'{0}: {1}'.format(context.line['user'], utils.unescape_html(answer.text)[:300])
-    else: return 'Sorry, no result.'
-
+def wolframalpha(context):
+    if not context.args:
+        return wolframalpha.__doc__
+    
+    result = utils.make_request(url, params={'asynchronous': 'false', 'i': context.args}, timeout=10)
+    if type(result) is str:
+        return result
+    
+    try:
+        html = result.text
+        tree = BeautifulSoup.BeautifulSoup(html)
+    except Exception:
+        return 'Error decoding WolframAlpha response.'
+    
+    html = tree.prettify().decode('utf8')
+    
+    if re.search('sure what to do with your input', html):
+            return 'No results.'
+    
+    found = re.findall('"stringified":\s"([^"]+)"', html)
+    
+    if not found: 
+        return 'Couldn\'t find results, bug Steve for a proper API.'
+    
+    results = found[:2][-1].split('\\n')
+    
+    def format_result_nicely(result):
+        if letters_re.match(result):
+            return result.replace(' | ', '')
+        return result
+    
+    results = [format_result_nicely(r) for r in results]
+    
+    return ', '.join(results)
