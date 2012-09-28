fa8a58be13277865eadad520ec647e0b9a04c159
Ed Kellett <edk141@gmail.com>
2012-09-28 23:39:05 +0100

    add counting

--- RedditBot/plugins/silly.py
+++ RedditBot/plugins/silly.py
@@ -1,5 +1,39 @@
 from RedditBot import bot
 
-@bot.regex('!squatlylist')
+from random import seed, randint
+
+@bot.regex('^!squatlylist')
 def squatlylist(context):
     return 'squatly list: squatly'
+
+
+high = 0
+target = 0
+
+seed()
+
+def reset():
+    global target
+    target = randint(2, 4)
+
+@bot.regex('.*')
+def counting(context):
+    global high
+    try:
+        next = int(context.args)
+        if next == 1:
+            print 'a'
+            reset()
+            high = 0
+        if next != high + 1:
+            print 'b'
+            high = 0
+        else:
+            print 'c'
+            high = next
+            if next + 1 == target:
+                print 'd'
+                high += 1
+                return str(high)
+    except ValueError:
+        high = 0
