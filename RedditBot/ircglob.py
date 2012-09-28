65873b8642a4a033c3e06e5d4bf0e8286da601e7
Ed Kellett <edk141@gmail.com>
2012-09-28 23:13:04 +0100

    simplify ircglob module

--- RedditBot/ircglob.py
+++ RedditBot/ircglob.py
@@ -9,27 +9,18 @@ def matches(pattern, prefix):
 class glob(object):
     '''Represents a nick!user@host glob pattern'''
     
-    invalid = False
-    
     def __init__(self, pattern):
-        if glob.is_valid(pattern):
-            self.nick, self.user, self.host = self.str_to_tuple(pattern)
-        else:
-            self.invalid = True
+        self.pattern = pattern
     
     @staticmethod
     def is_valid(nick_user_host):
         return glob.str_to_tuple(nick_user_host) != False
     
     def __eq__(self, other):
-        if self.invalid:
-            return False
-        return (self.nick, self.user, self.host) == (other.nick, other.user, other.host)
+        return self.isequiv(other)
     
     def __repr__(self):
-        if self.invalid:
-            return False
-        return 'glob({})'.format(repr((self.nick, self.user, self.host)))
+        return 'glob({})'.format(repr(self.pattern))
     
     @staticmethod
     def str_to_tuple(nick_user_host):
@@ -54,17 +45,9 @@ class glob(object):
                 r += re.escape(bit.group())
         return '^%s$' % r
     
-    @staticmethod
-    def matches_piece(pattern, thing):
-        '''Return True if thing matches pattern'''
-        return re.match(glob.pattern_to_re(pattern), thing, re.I) != None
-    
     def matches(self, test):
         '''Return True if 'test' is matched by the pattern we represent'''
-        if self.invalid:
-            return False
-        nick, user, host = self.str_to_tuple(test)
-        return self.matches_piece(self.nick, nick) and self.matches_piece(self.user, user) and self.matches_piece(self.host, host)
+        return re.match(glob.pattern_to_re(self.pattern), test, re.I) != None
     
     @staticmethod
     def pattern_to_super_re(pattern):
@@ -81,19 +64,14 @@ class glob(object):
                 r += re.escape(bit.group())
         return '^%s$' % r
     
-    @staticmethod
-    def issuper_piece(a, b):
-        '''Like glob.matches_piece but for superset testing'''
-        return re.match(glob.pattern_to_super_re(a), b, re.I) != None
-    
     def issuper(self, other):
         '''Return True if the set of all nick!user@host matched by this glob is a superset of those matched by 'other\''''
-        if self.invalid:
-            return False
-        return self.issuper_piece(self.nick, other.nick) and self.issuper_piece(self.user, other.user) and self.issuper_piece(self.host, other.host)
+        return re.match(glob.pattern_to_super_re(self.pattern), other.pattern, re.I) != None
     
     def issub(self, other):
         '''Return True if the set of all nick!user@host matched by this glob is a subset of those matched by 'other\''''
-        if self.invalid:
-            return False
         return other.issuper(self)
+    
+    def isequiv(self, other):
+        '''Return True if this glob is exactly equivalent to 'other\''''
+        return self.issuper(other) and self.issub(other)
