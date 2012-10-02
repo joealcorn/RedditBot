d8be1edbe7cfa9cc43429d31d523aba29ec9ea5c
Joe Alcorn <me@buttscicl.es>
2012-10-02 22:21:19 +0100

    Handle sigint with a QUIT :

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from RedditBot import bot
 
+from time import time, sleep
+import signal
+import sys
+
+
+def signal_handler(signal, frame):
+    bot.irc.send_command('QUIT :CTRL+C at Console')
+    sleep(1)
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-    from time import time
+    signal.signal(signal.SIGINT, signal_handler)
     bot.data['START_TIME'] = time()
     bot.run()
