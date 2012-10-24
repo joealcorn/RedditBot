3ac383f3297076d15226dbff8cca505682ef0c52
Joe Alcorn <me@buttscicl.es>
2012-10-24 09:57:41 +0100

    Move generate_insult from wolframalpha plugin to utils.py

--- RedditBot/utils.py
+++ RedditBot/utils.py
@@ -2,6 +2,7 @@
 from RedditBot.ircglob import glob
 from itertools import imap, ifilter
 from HTMLParser import HTMLParser
+import random
 import requests
 
 headers = {'User-Agent': 'irc.gamesurge.net #redditmc/RedditBot'}
@@ -64,3 +65,95 @@ def shorten_url(bot, url):
 
 def unescape_html(string):
     return HTMLParser().unescape(string)
+
+
+### begin insult code, shamelessly stolen from rbot
+
+adj = [
+"acidic", "antique", "contemptible",
+"culturally-unsound", "despicable",
+"evil", "fermented", "festering",
+"foul", "fulminating", "humid",
+"impure", "inept", "inferior",
+"industrial", "left-over",
+"low-quality", "malodorous",
+"off-color", "penguin-molesting",
+"petrified", "pointy-nosed", "salty",
+"sausage-snorfling", "tastless",
+"tempestuous", "tepid", "tofu-nibbling",
+"unintelligent", "unoriginal", "uninspiring",
+"weasel-smelling", "wretched",
+"spam-sucking", "egg-sucking",
+"decayed", "halfbaked", "infected",
+"squishy", "porous", "pickled",
+"coughed-up", "thick", "vapid",
+"hacked-up", "unmuzzled", "bawdy",
+"vain", "lumpish", "churlish",
+"fobbing", "rank", "craven",
+"puking", "jarring", "fly-bitten",
+"pox-marked", "fen-sucked",
+"spongy", "droning", "gleeking",
+"warped", "currish", "milk-livered",
+"surly", "mammering", "ill-borne",
+"beef-witted", "tickle-brained",
+"half-faced", "headless",
+"wayward", "rump-fed", "onion-eyed",
+"beslubbering", "villainous",
+"lewd-minded", "cockered", "full-gorged",
+"rude-snouted", "crook-pated",
+"pribbling", "dread-bolted", "fool-born",
+"puny", "fawning", "sheep-biting",
+"dankish", "goatish", "weather-bitten",
+"knotty-pated", "malt-wormy",
+"saucyspleened", "motley-mind",
+"it-fowling", "vassal-willed", "loggerheaded",
+"clapper-clawed", "frothy",
+"ruttish", "clouted",
+"common-kissing", "pignutted",
+"folly-fallen", "plume-plucked",
+"flap-mouthed", "swag-bellied",
+"dizzy-eyed", "gorbellied", "weedy",
+"reeky", "measled", "spur-galled",
+"mangled", "impertinent", "bootless",
+"toad-spotted", "hasty-witted",
+"horn-beat", "yeasty", "boil-brained",
+"tottering", "hedge-born", "hugger-muggered",
+"elf-skinned"
+]
+
+amt = [
+"accumulation", "bucket", "coagulation",
+"enema-bucketful", "gob", "half-mouthful",
+"heap", "mass", "mound", "petrification",
+"pile", "puddle", "stack", "thimbleful",
+"tongueful", "ooze", "quart", "bag",
+"plate", "ass-full", "assload"
+]
+
+noun = [
+"bat toenails", "bug spit", "cat hair",
+"chicken piss", "dog vomit", "dung",
+"fat-woman's stomach-bile", "fish heads",
+"guano", "gunk", "pond scum", "rat retch",
+"red dye number-9", "Sun IPC manuals",
+"waffle-house grits", "yoo-hoo", "dog balls",
+"seagull puke", "cat bladders", "pus",
+"urine samples", "squirrel guts", "snake assholes",
+"snake bait", "buzzard gizzards", "cat-hair-balls",
+"rat-farts", "pods", "armadillo snouts",
+"entrails", "snake snot", "eel ooze",
+"slurpee-backwash", "toxic waste", "Stimpy-drool",
+"poopy", "poop", "craptacular carpet droppings",
+"jizzum", "cold sores", "anal warts"
+]
+
+
+def generate_insult():
+    adj1 = random.choice(adj)
+    adj2 = adj1
+    while adj2 == adj1:
+        adj2 = random.choice(adj)
+
+    amt1 = random.choice(amt)
+    noun1 = random.choice(noun)
+    return '{0} {1} of {2} {3}'.format(adj1, amt1, adj2, noun1)
