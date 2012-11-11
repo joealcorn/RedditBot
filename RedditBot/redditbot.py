3cde45ae94d621f969c7b9c460407943a0b8ac87
Joe Alcorn <me@buttscicl.es>
2012-11-11 17:17:30 +0000

    Fix annoying pep8 whitespace errors

--- RedditBot/redditbot.py
+++ RedditBot/redditbot.py
@@ -25,20 +25,20 @@ class PluginHandler(irctk.plugins.PluginHandler):
         if glob.is_valid(prefix) and isignored(prefix, self.bot) and not isadmin(prefix, self.bot):
             return
         super(PluginHandler, self).enqueue_plugin(plugin, hook, context, regex)
-    
+
     # unfortunately we have to override this to catch an exception at func()
     def dequeue_plugin(self, plugin, plugin_context):
         for func in plugin['funcs']:
             takes_args = inspect.getargspec(func).args
-            
+
             action = False
             if plugin.get('action') == True:
                 action = True
-            
+
             notice = False
             if plugin.get('notice') == True:
                 notice = True
-            
+
             try:
                 if takes_args:
                     message = func(plugin_context)
@@ -58,11 +58,12 @@ class PluginHandler(irctk.plugins.PluginHandler):
                 except:
                     self.bot.log((plg, 'EXCEPTION', 'PASTE_FAILED'), etype.__name__, baby)
                 return
-            
+
             if message:
                 self._reply(message, plugin_context.line, action, notice)
 
 blacklist = ['REGEX', 'IGNORE', 'EVENTS', 'PLUGINS', 'START_TIME']
+
 
 class Bot(irctk.bot.Bot):
     h_config = None
@@ -72,13 +73,13 @@ class Bot(irctk.bot.Bot):
         self.reply_hook = None
         super(Bot, self).__init__()
         self.plugin = PluginHandler(self)
-    
+
     def save_config(self):
         with open('bot_config.yml', 'w') as f:
             f.write(yaml.dump(dict((key, value) for key, value in self.config.iteritems() if
                 not (key.upper() in blacklist) and
                 (not key in self.h_config or self.h_config[key] != value))))
-    
+
     def load_config(self):
         if os.path.exists('bot_config.yml'):
             with open('bot_config.yml', 'r') as f:
@@ -87,7 +88,7 @@ class Bot(irctk.bot.Bot):
                         self.config[key] = value
                     elif isinstance(self.config[key], list):
                         self.config[key] = list(set(self.config[key] + value))
-    
+
     def format_for_log(self, thing):
         if isinstance(thing, basestring):
             return thing
@@ -101,11 +102,12 @@ class Bot(irctk.bot.Bot):
             return u', '.join(thing)
         else:
             return unicode(thing)
-    
+
     def log(self, *args):
-        if not self.config['SNOOP_CHANNEL']: return
+        if not self.config['SNOOP_CHANNEL']:
+            return
         self.irc.send_message(self.config['SNOOP_CHANNEL'], u' '.join(self.format_for_log(x) for x in args))
-    
+
     def set_reply_hook(self, hook):
         self.reply_hook = hook
 
@@ -116,30 +118,30 @@ class Bot(irctk.bot.Bot):
             message = self.reply_hook(message)
         if not message:
             return
-        
+
         message = message[:max_lines * line_limit]
-        
+
         # split into lines
         for message in message.split('\n'):
             message = message.rstrip('\r')
             super(Bot, self).reply(message, context, action, notice, recipient, line_limit)
-    
+
     def inject_input(self, line):
         self.irc.connection.inp.put(line + '\r\n')
-    
+
     def _parse_input(self, wait=0.01):
         while True:
             time.sleep(wait)
             with self.irc.lock:
                 prefix = self.config['CMD_PREFIX']
-                
+
                 args = self.irc.context.get('args')
                 command = self.irc.context.get('command')
                 message = self.irc.context.get('message')
                 raw = self.irc.context.get('raw')
-                
+
                 while not self.context_stale and args:
-                    
+
                     # process regex
                     for regex in self.config['REGEX']:
                         hook = regex['hook']
@@ -152,7 +154,7 @@ class Bot(irctk.bot.Bot):
                                                    hook,
                                                    args[-1],
                                                    regex=True)
-                    
+
                     # process for a message
                     if message.startswith(prefix):
                         cmd = message[1:].split(' ')[0].lower()
@@ -173,13 +175,13 @@ class Bot(irctk.bot.Bot):
                             if match:
                                 hook = message.split(' ')[0]
                                 self.plugin.enqueue_plugin(match, hook, message)
-                    
+
                     # process for a command
                     if command and command.isupper():
                         for event in self.config['EVENTS']:
                             event['context'] = dict(self.irc.context)
                             hook = event['hook']
                             self.plugin.enqueue_plugin(event, hook, command)
-                    
+
                     # irc context consumed; mark it as such
                     self.irc.context['stale'] = True
