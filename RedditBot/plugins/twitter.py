2dbf1d77f175018766a4d63c6401dd9b04733485
Joe Alcorn <me@buttscicl.es>
2013-04-18 18:10:22 +0100

    Use version 1.1 of Twitter's API.
    This requires us to authenticate, 1.0 is due for shut down soon so it has to be done

--- RedditBot/plugins/twitter.py
+++ RedditBot/plugins/twitter.py
@@ -1,11 +1,56 @@
 from RedditBot import bot, utils
 
 import re
+import json
+import oauth2 as oauth
 
 tweet_re = re.compile('https?://(?:www\.|mobile\.)?twitter.com/.+/status(?:es)?/([0-9]{18})')
-status_url = 'https://api.twitter.com/1/statuses/show/{}.json'
-latest_url = 'http://api.twitter.com/1/statuses/user_timeline.json'
+status_url = 'https://api.twitter.com/1.1/statuses/show/{}.json'
+latest_url = 'http://api.twitter.com/1.1/statuses/user_timeline.json'
 line = u'@{screen_name}: {tweet}'
+
+
+class Tweet(object):
+
+    _consumer = oauth.Consumer(
+        key=bot.config['TWITTER_CONSUMER_KEY'],
+        secret=bot.config['TWITTER_CONSUMER_SECRET']
+    )
+    _token = oauth.Token(
+        key=bot.config['TWITTER_ACCESS_KEY'],
+        secret=bot.config['TWITTER_ACCESS_SECRET']
+    )
+    client = oauth.Client(_consumer, _token)
+
+    @classmethod
+    def make_request(self, url, params=None):
+        if params is not None:
+            # As far as I can tell oauth2.client doesn't support
+            # adding arbritrary params to the url like requests,
+            # so we do it manually
+            query_string = '&'.join(k + '=' + str(v) for k, v in params.items())
+            url = '{0}?{1}'.format(url, query_string)
+
+        try:
+            resp, body = self.client.request(url, 'GET')
+        except Exception:
+            return None
+        else:
+            return json.loads(body), int(resp['status'])
+
+    @classmethod
+    def by_id(self, id):
+        url = status_url.format(id)
+        return self.make_request(url, {'include_entities': 1})
+
+    @classmethod
+    def latest(self, username):
+        params = {
+            'screen_name': username,
+            'count': 1,
+            'include_entities': 1
+        }
+        return self.make_request(latest_url, params)
 
 
 def extract_info(json):
@@ -35,13 +80,15 @@ def extract_info(json):
 def announce_tweet(context):
     ''' Announces tweets as they are posted in the channel '''
     tweet_id = context.line['regex_search'].group(1)
-    url = status_url.format(tweet_id)
-    response = utils.make_request(url, params={'include_entities': 1})
-    if isinstance(response, str):
-        return response
+    tweet, code = Tweet.by_id(tweet_id)
 
-    tweet = extract_info(response.json)
+    if not tweet:
+        # Exception, fail silently so as not to spam the channel
+        return
+    elif code != 200:
+        return 'Something went wrong ({0})'.format(code)
 
+    tweet = extract_info(tweet)
     if tweet['screen_name'].lower() in bot.config['TWITTER_BLACKLIST']:
         return
 
@@ -57,20 +104,14 @@ def twitter(context):
     elif username.lower().lstrip('@') in bot.config['TWITTER_BLACKLIST']:
         return 'No.'
 
-    params = {
-        'screen_name': username,
-        'count': 1,
-        'include_entities': 1
-    }
+    tweet, code = Tweet.latest(username)
+    if tweet is None:
+        # Exception occcured
+        return
+    elif code == 404:
+        return "{0}: That user hasn't tweeted yet".format(context.line['user'])
+    elif code != 200:
+        return 'Something went wrong ({0})'.format(code)
 
-    response = utils.make_request(latest_url, params)
-    if isinstance(response, str):
-        return response
-    elif response.status_code == 404:
-        return 'Nonexistent user'
-    elif response.json == []:
-        return 'That user hasn\'t tweeted yet'
-
-    tweet = extract_info(response.json[0])
-
+    tweet = extract_info(tweet[0])
     return utils.unescape_html(line.format(**tweet))
